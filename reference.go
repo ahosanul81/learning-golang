1131c91fbc39f2686621bbf7a3719cc2db5a855b
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-10-23 10:06:39 +0600

    reference

---
+++ reference.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func callByReference(p *int, val int) {
+	*p = val
+}
+
+func main() {
+	var p *int
+	x := 4
+
+	p = &x
+	*p = *p - 1
+
+	callByReference(&x, 40)
+
+	fmt.Printf("x= %v\n", x)
+	fmt.Printf("memory addess of x = %v\n", p)
+	fmt.Printf("value of p = %v\n", *p)
+}
